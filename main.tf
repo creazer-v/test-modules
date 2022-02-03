7035cb48398c8a60e805323cf7163a2089113161
Mahendra M <61999561+creazer-v@users.noreply.github.com>
2022-02-03 16:55:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-##
 terraform {
 
   required_version = ">= 1.0"
@@ -23,68 +22,53 @@ provider "aws" {
 
 resource "aws_cognito_user_pool" "pool" {
   name = "test-userpool2335"
-  account_recovery_setting = [
-    {
-      recovery_mechanism = [
-        {
-          name     = "verified_email",
-          priority = 1
-        },
-        {
-          name     = "verified_phone_number",
-          priority = 2
+  account_recovery_setting {
+    recovery_mechanism {
+        name   = "verified_email"
+        priority = "1"
         }
-      ]
+    recovery_mechanism {
+        name     = "verified_phone_number"
+        priority = 2
+        }
+ }
+admin_create_user_config{
+    allow_admin_create_user_only = "false"
     }
-  ]
-  admin_create_user_config = [
-    {
-      allow_admin_create_user_only = false
-    }
-  ]
+
   alias_attributes = [
-    "email"
-  ]
+  "email"]
   auto_verified_attributes = [
     "email"
   ]
-  email_configuration = [
-    {
-      email_sending_account = "COGNITO_DEFAULT",
-    }
-  ]
-  password_policy = [
-    {
-      minimum_length                   = 8,
-      require_lowercase                = true,
-      require_numbers                  = true,
-      require_symbols                  = true,
-      require_uppercase                = true,
-      temporary_password_validity_days = 7
-    }
-  ]
-  schema = [
-    {
-      attribute_data_type          = "String",
-      developer_only_attribute     = false,
-      mutable                      = true,
-      name                         = "email",
-      number_attribute_constraints = [],
-      required                     = true,
-      string_attribute_constraints = [
-        {
-          max_length = "2048",
-          min_length = "0"
-        }
-      ]
-    }
-  ]
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+
+  }
+  password_policy {
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
+    temporary_password_validity_days = 7
+  }
+  schema {
+    attribute_data_type          = "String"
+    developer_only_attribute     = false
+    mutable                      = true
+    name                         = "email"
+    required                     = true
+    string_attribute_constraints{
+      
+        max_length = "2048"
+        min_length = "0"
+      }
+  }
   mfa_configuration = "OFF"
-  username_configuration = [
-    {
-      case_sensitive = false
-    }
-  ]
+  username_configuration {
+    case_sensitive = false
+  }
 }
 
 resource "aws_cognito_user_pool_client" "client" {
